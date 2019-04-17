ff3faf8f4079b81c997a4fe061668832ebd30da5
reeshkeed <girishdaloso@hotmail.com>
2019-04-17 14:16:05 +0800

    Created a knockout text

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,8 +2,12 @@
   <div class="home">
     <div class="img-cover text-centered text-white">
       <div class="">
-        <!--h1 id="me">GIRISH</h1>
-        <h1 id="me">DALOSO</h1-->
+        <h1 class="me">GIRISH</h1>
+        <h1 class="me">DALOSO</h1>
+        <div class="">
+          <p id="designer-text">{{ designer }}</p>
+          <p id="developer-text">{{ developer }}</p>
+        </div>
       </div>
     </div>
     <h1>test</h1>
@@ -20,7 +24,8 @@ export default {
   name: "Home",
   data() {
     return {
-      description: "Designer / <developer>"
+      designer: "Designer",
+      developer: "<developer>"
     };
   }
 };
@@ -35,11 +40,29 @@ export default {
   margin-top: -5.7rem
   height: 100%
 
-#me
-  font-family: 'Code Light'
+.me
+  -webkit-background-clip: text
+  margin: 0
+  color: transparent
   font-size: 8rem
   text-align: center
-  color: #8ba0fa
+  font-family: 'Monoton', cursive
+  font-weight: 400
+  background-image: url('/static/img/bg.svg')
+  background-repeat: no-repeat
+  background-position: center
+  background-size: cover
+
+#designer-text, #developer-text
+  font-size: 1.5rem
+  color: #434343
+  display: inline-block
+
+#designer-text
+  font-family: 'Norican', cursive
+
+#developer-text
+  font-family: 'Inconsolata', monospace
 
 #description
   font-size: 2rem
