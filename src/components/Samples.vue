0415d67b3191d142f4f6c38e5aec6bfaf20b9f8a
reeshkeed <girishdaloso@hotmail.com>
2019-05-01 22:37:34 +0800

    Added one project sample

--- src/components/Samples.vue
+++ src/components/Samples.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="samples">
-    <h1>Samples</h1>
+    <div class="container">
+      <h1 class="title">Sample</h1>
+
+      <div class="col-lg-6 col-xs-12">
+        <a href="#" class="card-link">
+          <div class="card-box">
+            <div class="card-content">
+              <h2 class="card-title">Xhander's</h2>
+              <p class="card-description">A sample for restaurant website.</p>
+            </div>
+            <div class="card-image fourth-project"></div>
+          </div>
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
