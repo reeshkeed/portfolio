e16df59715681864e756e1d88667db6c736b11ac
reeshkeed <girishdaloso@hotmail.com>
2019-05-03 00:32:38 +0800

    Added rizal website page

--- src/components/work/Rizal.vue
+++ src/components/work/Rizal.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="rizal">
     <div class="container">
-      <h1 class="title">Rizal</h1>
+      <div class="project">
+        <div class="brief">
+          <h2 class="brief-title">Jose Rizal Website</h2>
+          <p class="role">Designer / Developer</p>
+        </div>
+        <div class="line"></div>
+
+        <div class="project-background">
+          <h2 class="subtitle">About the Project</h2>
+          <p class="description">This website <a class="link" href="https://joserizalwebsite.herokuapp.com/index.html">(visit site here)</a>
+            contains information about Jose Rizal's life and works. It aims to help our fellow students
+            and other people who need or having a research about Rizal's life. It contains the details of his different
+            characteristics, his family, his travels abroad to study, his poems, philosophies, achievements, and
+            other works and accomplishments . </p>
+
+          <h2 class="subtitle">Technologies</h2>
+          <ul class="tools">
+            <li>Bootsrap</li>
+            <li>jQuery</li>
+          </ul>
+
+          <h2 class="subtitle">View</h2>
+          <img class="view" src="/static/img/projects/rizal_web.png" alt="">
+        </div>
+      </div>
     </div>
   </div>
 </template>
