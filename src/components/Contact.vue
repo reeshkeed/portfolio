89e8a11a8911f47bf310e1cb221801811b46a2e2
reeshkeed <girishdaloso@hotmail.com>
2019-05-03 01:56:53 +0800

    Finish contact page

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="contact">
-    <h1>Contact</h1>
+    <div class="fullscreen v-centered">
+      <div class="container">
+        <h2 id="contact-title">Hi there!</h2>
+
+        <p id="contact-subtitle">Interested working with me?</p>
+        <p id="contact">send an email to <a href="mailto:girishdaloso@hotmail.com" id="email">girishdaloso@hotmail.com</a></p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,3 +16,35 @@ export default {
   name: "Contact"
 };
 </script>
+
+<style lang="sass">
+.contact
+  margin-top: -8.7rem
+
+#contact-title
+  font-weight: 300
+  font-size: 4rem
+
+#contact-subtitle
+  font-size: 2rem
+  color: #434343
+
+#contact
+  font-size: 1.4rem
+  font-weight: 100
+
+#email
+  color: #aba9e7
+  font-weight: 400
+  text-decoration: none
+
+@media (max-width: 768px)
+  #contact-title
+    font-size: 3rem
+
+  #contact-subtitle
+    font-size: 1.6rem
+
+  #contact
+    font-size: 1.2rem
+</style>
