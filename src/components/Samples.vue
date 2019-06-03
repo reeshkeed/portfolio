2ff7dd37735d76e5ccf74e09b00419506b352a5c
reeshkeed <girishdaloso@hotmail.com>
2019-06-03 23:09:18 +0800

    Revert google analytics added script use vue analytics

--- src/components/Samples.vue
+++ src/components/Samples.vue
@@ -21,17 +21,6 @@
       </router-link>
     </div>
   </div>
-
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141297455-1"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-141297455-1');
-  </script>
-
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
 import App from './App'
 import router from './router'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-141298949-1',
+  router
+})
 
 Vue.config.productionTip = false
 
