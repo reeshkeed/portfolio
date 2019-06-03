540f03be64713094c4400e4e92da1955ed1cff72
reeshkeed <girishdaloso@hotmail.com>
2019-06-03 16:28:16 +0800

    WIP Added google analytics script

--- src/components/work/Icon.vue
+++ src/components/work/Icon.vue
@@ -30,6 +30,16 @@
       </div>
     </div>
   </div>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141297455-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-141297455-1');
+  </script>
 </template>
 
 <script>
